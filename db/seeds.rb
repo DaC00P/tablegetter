7af1076b302d700867c0a4b855cf4ff7b2aae844
Daniel Cuperman <dacooperfish@gmail.com>
2016-07-01 13:29:05 -0700

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,15 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Restaurant.create!((name: "", chef: "", cuisine: "", capacity: , description: ""))
+Restaurant.create!(name: "Osteria Francescana", chef: "Massimo Bottura", cuisine: "Italian", capacity: 40, description: "A hyper-modern take on classic Italian cuisine")
+Restaurant.create!(name: "Blue Hill Restaurant", chef: "Dan Barber", cuisine: "Local Farm to Table - New York", capacity: 100, description: "A farmer's feast of beautiful and respectfully raised local produce")
+Restaurant.create!(name: "El Restaurante Patagonia Sur", chef: "Francis Mallman", cuisine: "Argentinian & Patagonian", capacity: 50, description: "A modern haute-cuisine presentation of Patagonia & Argentina's bounty")
+Restaurant.create!(name: "N/Naka", chef: "Niki Nakayama", cuisine: "Japanese", capacity: 35, description: "A modern Kaiseki rendition that focuses on a story told through its dishes")
+Restaurant.create!(name: "Attica", chef: "Ben Shewry", cuisine: "Australian", capacity: 25, description: "A neo-modern representation of Australia's native ingredients")
+Restaurant.create!(name: "Favikan", chef: "Magnus Nilsson", cuisine: "Swedish", capacity: 20, description: "Swedish traditional techniques and local produce combine with modern French techniques to create a unique experience")
+Restaurant.create!(name: "Alinea", chef: "Grant Achatz", cuisine: "Modern American", capacity: 16, description: "An experience in modern Art & Food - expect to be shocked and thrilled!")
+Restaurant.create!(name: "D.O.M.", chef: "Alex Atala", cuisine: "Brazilian", capacity: 80, description: "A stunning modernist take on traditional Brazilian ingredients and recipes")
+Restaurant.create!(name: "Atelier Crenn", chef: "Dominique Crenn", cuisine: "French", capacity: 100, description: "A journey through Dominique's life, represented in her beautiful and delicious creations")
+Restaurant.create!(name: "Pujol", chef: "Enrique Olivera", cuisine: "Mexican", capacity: 34, description: "Traditional Mexican recipies and techniques transported into the finest modern cuisine")
+Restaurant.create!(name: "Hisa Franko", chef: "Ana Ros", cuisine: "Slovenian", capacity: 40, description: "A beautiful transformation of traditional Slovenian fare")
+Restaurant.create!(name: "Gaggan", chef: "Gaggan Anand", cuisine: "Indian", capacity: 100, description: "Wonderous Indian dishes based on tradition, but twisted into hyper-modern transformations")

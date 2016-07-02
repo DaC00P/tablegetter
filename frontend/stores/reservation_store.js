bbdcb7366c5e8360cb6f722b9ba4afcc2517d7ca
DaC00P <dacuperman@gmail.com>
2016-07-01 17:12:53 -0700

    reservation flux loop, untested, in place

---
+++ frontend/stores/reservation_store.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const Store = require('flux/utils').Store;
+const AppDispatcher = require('../dispatcher/dispatcher');
+const RestaurantStore = new Store(AppDispatcher);
+
+let _reservations = {}
+
+ReservationStore.__onDispatch = function(payload) {
+  switch (payload.actionType){
+    case "receive_reservation":
+      resetAllReservations(payload.reservation);
+      break;
+  }
+}
+
+ReservationStore.all = function() {
+  return Object.keys(_reservations).map((key) => {return (_reservations[key])})
+};
+
+ReservationStore.find = function(id) {
+  return Object.assign({}, _reservations[id])
+};
+
+function resetAllReservations(reservation) {
+  _reservations = reservation;
+  ReservationStore.__emitChange();
+}
+
+
+
+module.exports = ReservationStore;

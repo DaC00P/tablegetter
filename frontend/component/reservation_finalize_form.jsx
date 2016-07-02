dc9503280cd406df6e0c52d0c9259d25aa92ac9c
Daniel Cuperman <dacooperfish@gmail.com>
2016-07-01 17:12:53 -0700

    reservation flux loop, untested, in place

--- frontend/component/reservation_finalize_form.jsx
+++ frontend/component/reservation_finalize_form.jsx
@@ -8,22 +8,52 @@ const RestaurantStore = require('../stores/restaurant_store');
 const RestaurantActions = require('../actions/restaurant_actions');
 
 const ReservationFinalizeForm = React.createClass({
+  getInitialState() {
+    return ({
+      date: this.props.date.toString(),
+      time: this.props.time.value,
+      partySize: 0,
+      allergies: "",
+      specialInstructions: ""
+    })
+  },
+
+  handlePartySize(event) {
+    event.preventDefault();
+    this.setState({partySize: event.target.value})
+  },
+
+  handleAllergies(event) {
+    event.preventDefault();
+    this.setState({allergies: event.target.value})
+  },
+
+  handleInstructions(event) {
+    event.preventDefault();
+    this.setState({specialInstructions: event.target.value})
+  },
+
+  handleReservationSubmit() {
+    console.log(this.state)
+    //error check and send to store which sends to DB via ajax
+  },
+
   render() {
     return (
       <section className='reservation-finalize-form'>
         <div className='reservation-finalize-form'>
-          <h3>Please Complete Your Reservation UserName</h3>
+          <h3>Please Complete Your Reservation for </h3>
             Your Reservation Date: {this.props.date.toString()}
           <br/>
             Your Reservation Time: {this.props.time.value}
           <form>
-            <input type="text" placeholder="Please Enter Your Party Size" className="reservation-entry-details"/>
+            <input onChange={this.handlePartySize} type="text" placeholder="Please Enter Your Party Size" className="reservation-entry-details"/>
             <br/>
-            <input type="text" placeholder="Please Enter Any Allergies So We May Better Serve You" className="reservation-entry-details"/>
+            <input onChange={this.handleAllergies} type="text" placeholder="Please Enter Any Allergies So We May Better Serve You" className="reservation-entry-details"/>
             <br/>
-            <input type="text" placeholder="Please Enter Any Special Instructions So We May Better Serve You" className="reservation-entry-details"/>
+            <input onChange={this.handleInstructions} type="text" placeholder="Please Enter Any Special Instructions So We May Better Serve You" className="reservation-entry-details"/>
             <br/>
-            <input type="submit" value="Finalize Reservation"/>
+            <input onClick={this.handleReservationSubmit} type="submit" value="Finalize Reservation"/>
           </form>
         </div>
       </section>

78132608aa9e22dde791300812ffe094dc8ead1e
Daniel Cuperman <dacuperman@gmail.com>
2016-07-04 12:16:37 -0700

    reservation confirmation

--- frontend/component/reservation_finalize_form.jsx
+++ frontend/component/reservation_finalize_form.jsx
@@ -16,7 +16,8 @@ const ReservationFinalizeForm = React.createClass({
       party_size: 0,
       allergies: "",
       special_instructions: "",
-      restaurant_id: this.props.restaurant.id
+      restaurant_id: this.props.restaurant.id,
+      finalize: false
     })
   },
 
@@ -36,27 +37,56 @@ const ReservationFinalizeForm = React.createClass({
   },
 
   handleReservationSubmit() {
-    ReservationActions.postSingleReservation({reservation: this.state});
+    ReservationActions.postSingleReservation({reservation: this.state}, this.reservationConfirmed);
+  },
+
+  handleFinalizeClick() {
+    this.handleReservationSubmit();
+  },
+
+  reservationConfirmed() {
+    this.setState({finalize: true})
   },
 
   render() {
+
+    const confirmation = (
+      <div id="5">
+        <h3>Congratulations! You have completed your reservation for {this.props.restaurant.name}</h3>
+          Your Reservation Date: {this.props.date.toString()}
+        <br/>
+          Your Reservation Time: {this.props.time.value}
+      </div>
+    );
+
+    const reservationForm = (
+      <div className='reservation-finalize-form'>
+        <h3>Please Complete Your Reservation for {this.props.restaurant.name}</h3>
+          Your Reservation Date: {this.props.date.toString()}
+        <br/>
+          Your Reservation Time: {this.props.time.value}
+        <form>
+          <input onChange={this.handlePartySize} type="text" placeholder="Please Enter Your Party Size" className="reservation-entry-details"/>
+          <br/>
+          <input onChange={this.handleAllergies} type="text" placeholder="Please Enter Any Allergies So We May Better Serve You" className="reservation-entry-details"/>
+          <br/>
+          <input onChange={this.handleInstructions} type="text" placeholder="Please Enter Any Special Instructions So We May Better Serve You" className="reservation-entry-details"/>
+          <br/>
+          <input onClick={this.handleFinalizeClick} type="submit" value="Finalize Reservation"/>
+        </form>
+      </div>
+    );
+
+    let currentForm = reservationForm;
+
+    if (this.state.finalize) {
+      currentForm = confirmation;
+    };
+
+
     return (
       <section className='reservation-finalize-form'>
-        <div className='reservation-finalize-form'>
-          <h3>Please Complete Your Reservation for {this.props.restaurant.name}</h3>
-            Your Reservation Date: {this.props.date.toString()}
-          <br/>
-            Your Reservation Time: {this.props.time.value}
-          <form>
-            <input onChange={this.handlePartySize} type="text" placeholder="Please Enter Your Party Size" className="reservation-entry-details"/>
-            <br/>
-            <input onChange={this.handleAllergies} type="text" placeholder="Please Enter Any Allergies So We May Better Serve You" className="reservation-entry-details"/>
-            <br/>
-            <input onChange={this.handleInstructions} type="text" placeholder="Please Enter Any Special Instructions So We May Better Serve You" className="reservation-entry-details"/>
-            <br/>
-            <input onClick={this.handleReservationSubmit} type="submit" value="Finalize Reservation"/>
-          </form>
-        </div>
+        {currentForm}
       </section>
     )
   }

e072aa295c9398999d40314492d2b5755384c648
Daniel Cuperman <dacooperfish@gmail.com>
2016-07-07 21:29:12 -0700

    reviews almost done

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160708001929) do
+ActiveRecord::Schema.define(version: 20160708030919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20160708001929) do
     t.string   "city"
     t.text     "chef_pic_url"
     t.text     "restaurant_cover_pic"
+    t.text     "reviews"
   end
 
   create_table "reviews", force: :cascade do |t|
